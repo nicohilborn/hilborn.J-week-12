d9545bdb94c274957b7274cae4877801fb928061
nicohilborn <nicohilborn@gmail.com>
2025-11-06 16:02:51 -0600

    code

---
+++ function.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.getElementById("gallery");
+  const box = document.querySelector(".box");
+  const colorButton = document.getElementById("changeColor");
+  const toggleButton = document.getElementById("toggleImage");
+
+  colorButton.addEventListener("click", () => {
+    box.style.backgroundColor = box.style.backgroundColor === "blue" ? "red" : "blue";
+  });
+
+
+
+  let toggled = false;
+  toggleButton.addEventListener("click", () => {
+    if (toggled) {
+      gallery.src = "./assets/pumpkin.png";
+      toggled = false;
+    } else {
+      gallery.src = "./assets/ghost.png";
+      toggled = true;
+    }
+  });
+  console.log("Page loaded!");
+});
